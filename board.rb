75a84f79dac2d1861dbcde997166f4f66ba83165
mxrgxr <mgxrcias@gmail.com>
2023-09-05 13:58:24 -0700

    user friendly board visualization

--- board.rb
+++ board.rb
@@ -6,4 +6,13 @@ class Board
     # inital board has 2D array with 3 "rows" each containing 3 cells of empty string
     @board = Array.new(3) { Array.new(3, ' ') }
   end
+
+  def display
+    # iterate over board array with row indices to display lines
+    @board.each_with_index do |row, index|
+      puts row.join(' | ')
+      # prevent line below last row
+      puts '---------' unless index == @board.length - 1
+    end
+  end
 end
