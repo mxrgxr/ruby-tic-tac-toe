562eed23f3bf9702d0d94011e69b926c3cc2ab66
mxrgxr <mgxrcias@gmail.com>
2023-09-05 13:50:19 -0700

    initialize board

--- board.rb
+++ board.rb
@@ -2,5 +2,8 @@
 
 # Initialize, render, update board
 class Board
-
+  def initialize
+    # inital board has 2D array with 3 "rows" each containing 3 cells of empty string
+    @board = Array.new(3) { Array.new(3, ' ') }
+  end
 end
