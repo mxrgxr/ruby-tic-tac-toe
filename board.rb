15dd94c4ea2bf141b47418e4c2f638cf1cca2fd6
mxrgxr <mgxrcias@gmail.com>
2023-09-05 14:10:39 -0700

    change board to 9 cells instead of 2d array

--- board.rb
+++ board.rb
@@ -3,16 +3,15 @@
 # Initialize, render, update board
 class Board
   def initialize
-    # inital board has 2D array with 3 "rows" each containing 3 cells of empty string
-    @board = Array.new(3) { Array.new(3, ' ') }
+    # inital board has array with 9 cells
+    @board = Array.new(9, ' ')
   end
 
   def display
-    # iterate over board array with row indices to display lines
-    @board.each_with_index do |row, index|
+    # Iterate over the board array and display lines
+    @board.each_slice(3).with_index do |row, index|
       puts row.join(' | ')
-      # prevent line below last row
-      puts '---------' unless index == @board.length - 1
+      puts '---------' unless index == 2
     end
   end
 end
