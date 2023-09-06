c3376b1e38aeaa57544e474d9385461baa7c2a29
mxrgxr <mgxrcias@gmail.com>
2023-09-05 21:04:10 -0700

    handle player move on board

--- player.rb
+++ player.rb
@@ -6,4 +6,9 @@ class Player
     # player id is X or O
     @symbol = symbol
   end
+
+  def make_move(board, cell_id)
+    # board update method called and passed in cell to fill with symbol
+    board.update(cell_id, @symbol)
+  end
 end
