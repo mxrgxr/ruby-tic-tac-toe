4dadabecfd6933556956b727905add403620df5d
mxrgxr <mgxrcias@gmail.com>
2023-09-05 21:25:51 -0700

    update player class to check if player is AI

--- player.rb
+++ player.rb
@@ -1,14 +1,22 @@
 # frozen_string_literal: true
 
+require_relative 'ai'
 # Player symbol and handle user's move
 class Player
-  def initialize(symbol)
+  include AI
+
+  def initialize(symbol, is_ai: false)
     # player id is X or O
     @symbol = symbol
+    @is_ai = is_ai
   end
 
-  def make_move(board, cell_id)
-    # board update method called and passed in cell to fill with symbol
-    board.update(cell_id, @symbol)
+  def make_move(board, cell_id = nil)
+    if @is_ai
+      AI.make_move(board, @symbol)
+    else
+      # board update method called and passed in cell to fill with symbol
+      board.update(cell_id, @symbol)
+    end
   end
 end
