e461a5285617d2e7674380626814af92d06f7841
mxrgxr <mgxrcias@gmail.com>
2023-09-05 21:31:53 -0700

    initialize game with player and board instantiation

--- game.rb
+++ game.rb
@@ -2,5 +2,12 @@
 
 # Handle game flow from start, handle player input, check for winner, and display messages
 class Game
-
+  def initialize
+    player_symbol = get_player_symbol
+    ai_symbol = player_symbol == 'X' ? 'O' : 'X'
+    @player1 = Player.new(player_symbol)
+    @player2 = Player.new(ai_symbol, is_ai: true)
+    @board = Board.new
+    @current_player = @player1
+  end
 end
